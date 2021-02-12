cea4a4ca19725ebfd7311bf918cdc322a85558c7
Vtalii Drahun <vdrag00n90@gmail.com>
2021-02-12 13:27:02 +0200

    Initial project structure

---
+++ src/main/java/org/vdragun/webfluxmongo/RestApiWithWebFluxAndMongoApplication.java
@@ -0,0 +1,13 @@
+package org.vdragun.webfluxmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiWithWebFluxAndMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiWithWebFluxAndMongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/vdragun/webfluxmongo/RestApiWithWebFluxAndMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vdragun.webfluxmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiWithWebFluxAndMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
